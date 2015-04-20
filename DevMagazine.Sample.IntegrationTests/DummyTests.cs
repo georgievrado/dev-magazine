a82711ccbe6a384edfd604eb916fef465e98f530
Radoslav Georgiev <radoslav.georgiev@telerik.com>
2015-04-20 12:24:06 +0300

    Add Integration tests project

---
+++ DevMagazine.Sample.IntegrationTests/DummyTests.cs
@@ -0,0 +1,51 @@
+﻿using MbUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMagazine.Sample.IntegrationTests
+{
+    public class DummyTests
+    {
+        [Test]
+        [Category("Dummy Tests")]
+        [Author("Radoslav Georgiev")]
+        [Description("A simple test that will pass.")]
+        public void PassingTest()
+        {
+            int expectedValue = 2;
+
+            int actualValue = 1 + 1;
+
+            Assert.AreEqual<int>(expectedValue, actualValue);
+        }
+
+        [Test]
+        [Category("Dummy Tests")]
+        [Author("Radoslav Georgiev")]
+        [Description("A simple test that will fail.")]
+        public void FailingTest()
+        {
+            int expectedValue = 2;
+
+            int actualValue = 1 + 1 + 1;
+
+            Assert.AreEqual<int>(expectedValue, actualValue);
+        }
+
+        [Test]
+        [Category("Dummy Tests")]
+        [Author("Radoslav Georgiev")]
+        [Description("A simple test that will be ignored. You can run it manually in the Test runner.")]
+        public void IgnoredTest()
+        {
+            int expectedValue = 2;
+
+            int actualValue = 1 + 1;
+
+            Assert.AreEqual<int>(expectedValue, actualValue);
+        }
+    }
+}
