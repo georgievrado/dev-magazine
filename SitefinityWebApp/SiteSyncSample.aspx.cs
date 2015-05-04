814739bdeb18ba9f80caea49aeb04bdc6652a439
avisra <chris@avisra.com>
2015-05-03 22:21:38 -0400

    Included SiteSyncSample webform

---
+++ SitefinityWebApp/SiteSyncSample.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Telerik.Sitefinity;
+using Telerik.Sitefinity.Events.Model;
+using Telerik.Sitefinity.SiteSync;
+
+namespace SitefinityWebApp
+{
+    public partial class SiteSyncSample : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            App.WorkWith().SiteSync().SelectTarget("destination-site-url.com").SetSites(new string[] { "Site A" }).Sync(new string[] { typeof(Event).FullName });
+        }
+    }
+}
